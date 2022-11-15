19fda155ee99aaa335c999dde74a196744ae2510
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-16 02:12:19 +0600

    insert appointmnet data in MongoDb and load server side UI

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +9,28 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+// mongoDB connections
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mpr3cem.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const appoinmentOptionsCollection = client.db("doctorsPortal").collection("appoinmentOptions");
+
+        // get all inseted data MongoDb
+        app.get('/appoinmentOptions', async (req, res) => {
+            const query = {};
+            const options = await appoinmentOptionsCollection.find(query).toArray();
+            res.send(options);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
 
 
 // Initial and Basic Setup
