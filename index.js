185267e34b1ea2733371a19eb5f9a8b54ad81e0d
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-22 03:57:40 +0600

    Server side payment intents API to get client Secret from stripe

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY); s
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -185,6 +186,25 @@ async function run() {
 
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
+        });
+
+
+        // using for stripe payment from: https://stripe.com/docs/payments/quickstart
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: "usd",
+                amount: amount,
+                "payment_methods_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            })
         });
 
         // get all user
